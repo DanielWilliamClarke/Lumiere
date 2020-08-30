a6b29e63e16c99623bdebdf383e3000ae90744b1
Daniel William Clarke <clarkit@gmail.com>
2020-08-30 08:11:50 +0100

    Refactored transfer so append records to transaction instead of updating the whole transaction array

--- src/account/account_transfer.go
+++ src/account/account_transfer.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -25,13 +26,16 @@ type TransferBody struct {
 
 func (a AccountTransferRoute) PutTransfer(c *fiber.Ctx) {
 
-	authedAccount := c.Locals("AuthedAccount").(*model.Account)
-	if authedAccount == nil {
-		log.Printf("Auth user id is nil")
+	ctx := context.Background()
+
+	// Retrieve authed account from upstream
+	authedAccount, err := a.getAccount(c)
+	if err != nil {
 		c.Status(http.StatusInternalServerError).Send("Internal server error")
 		return
 	}
 
+	// Parse request body
 	body := &TransferBody{}
 	if err := c.BodyParser(body); err != nil {
 		log.Printf("Could not parse request body: %v", err)
@@ -39,57 +43,87 @@ func (a AccountTransferRoute) PutTransfer(c *fiber.Ctx) {
 		return
 	}
 
-	ctx := context.Background()
-
-	toAccount := &model.Account{}
-	err := a.DataAccess.FindOne(ctx, bson.M{"name": body.To}, toAccount)
+	// Retrieve receiptent from storage
+	receiptient, err := a.getReceiptient(ctx, body.To, authedAccount)
 	if err != nil {
-		log.Printf("Transfer receiptient does not exist: %v", err)
-		c.Status(http.StatusNotFound).Send("Internal server error")
+		c.Status(http.StatusNotFound).Send(err.Error())
 		return
 	}
-
-	if toAccount.ID == authedAccount.ID {
-		log.Printf("Transfer receiptient same as sender: %v", err)
-		c.Status(http.StatusBadRequest).Send("Cannot transfer to self")
-		return
-	}
-
-	authedAccount.Transactions = append(authedAccount.Transactions, model.Transaction{
-		Amount: -body.Amount,
-		From:   authedAccount.ID,
-		To:     toAccount.ID,
-		Date:   time.Now().Format("2006.01.02 15:04:05"),
-	})
-	toAccount.Transactions = append(toAccount.Transactions, model.Transaction{
-		Amount: body.Amount,
-		From:   authedAccount.ID,
-		To:     toAccount.ID,
-		Date:   time.Now().Format("2006.01.02 15:04:05"),
-	})
 
 	// Perform update in transaction so we can rollback all changes if any error occurs here
-	err = a.DataAccess.StartTransaction(ctx, func() error {
-		err = a.DataAccess.UpdateOne(ctx, bson.M{"_id": authedAccount.M_ID}, bson.D{{"$set", bson.M{"transactions": authedAccount.Transactions}}})
-		if err != nil {
-			log.Printf("Unable to update [from] account transactions: %v", err)
-			return err
-		}
-		err = a.DataAccess.UpdateOne(ctx, bson.M{"_id": toAccount.M_ID}, bson.D{{"$set", bson.M{"transactions": toAccount.Transactions}}})
-		if err != nil {
-			log.Printf("Unable to update [to] account transactions: %v", err)
-			return err
-		}
-		return nil
-	})
-
+	err = a.safeTransfer(ctx, authedAccount, receiptient, body.Amount)
 	if err != nil {
 		log.Printf("Account transfer failed: %v", err)
 		c.Status(http.StatusInternalServerError).Send("Internal server error")
 		return
 	}
 
+	// Render success output
 	timeFormat := time.Now().Format("2006.01.02 15:04:05")
-	transferReceipt := fmt.Sprintf("%s your transfer of $%.2f at %s to %s is complete", authedAccount.Name, body.Amount, timeFormat, toAccount.Name)
+	transferReceipt := fmt.Sprintf("%s your transfer of $%.2f at %s to %s is complete", authedAccount.Name, body.Amount, timeFormat, receiptient.Name)
 	c.Status(http.StatusOK).Send(transferReceipt)
+}
+
+func (a AccountTransferRoute) getAccount(c *fiber.Ctx) (*model.Account, error) {
+	authedAccount := c.Locals("AuthedAccount")
+	if authedAccount == nil {
+		reason := "Authed account is nil"
+		log.Printf(reason)
+		return nil, errors.New(reason)
+	}
+
+	return authedAccount.(*model.Account), nil
+}
+
+func (a AccountTransferRoute) getReceiptient(ctx context.Context, receiptientName string, authedAccount *model.Account) (*model.Account, error) {
+	// Retrive to receiptient account
+	receiptient := &model.Account{}
+	err := a.DataAccess.FindOne(ctx, bson.M{"name": receiptientName}, receiptient)
+	if err != nil {
+		log.Printf("Transfer receiptient does not exist: %v", err)
+		return nil, errors.New("Transfer receiptient does not exist")
+	}
+
+	// Guard agaisnt unary transfer
+	if receiptient.ID == authedAccount.ID {
+		log.Printf("Transfer receiptient same as sender: %v", err)
+		return nil, errors.New("Cannot transfer to self")
+	}
+
+	return receiptient, nil
+}
+
+func (a AccountTransferRoute) safeTransfer(ctx context.Context, authedAccount *model.Account, receiptient *model.Account, amount float64) error {
+	return a.DataAccess.StartTransaction(ctx, func() error {
+
+		err := a.DataAccess.UpdateOne(ctx,
+			bson.M{"_id": authedAccount.M_ID},
+			a.createTransactionUpdate(authedAccount, receiptient, -amount))
+		if err != nil {
+			log.Printf("Unable to update [from] account transactions: %v", err)
+			return err
+		}
+
+		err = a.DataAccess.UpdateOne(ctx,
+			bson.M{"_id": receiptient.M_ID},
+			a.createTransactionUpdate(authedAccount, receiptient, amount))
+		if err != nil {
+			log.Printf("Unable to update [to] account transactions: %v", err)
+			return err
+		}
+
+		return nil
+	})
+}
+
+func (a AccountTransferRoute) createTransactionUpdate(authedAccount *model.Account, receiptient *model.Account, amount float64) bson.D {
+	return bson.D{
+		{"$addToSet",
+			bson.M{"transactions": model.Transaction{
+				Amount: amount,
+				From:   authedAccount.ID,
+				To:     receiptient.ID,
+				Date:   time.Now().Format("2006.01.02 15:04:05"),
+			}},
+		}}
 }
